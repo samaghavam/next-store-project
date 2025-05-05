a309542c7e05b4908b6c054b66d27fb9423374f4
sama <sghavamzadeh46@gmail.com>
2025-05-05 16:17:34 +0330

    docs(file): add radxix packages and remove pre-commit file

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,12 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 export default function HomePage() {
   return(
-    <div>home pahe</div>
+    <div>
+      <h1 className="font-bold">home page</h1>
+      <Button asChild size='lg' variant='outline' className="m-8">
+        <Link href='/products'>products</Link>
+      </Button>
+    </div>
   )} 
