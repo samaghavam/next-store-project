147bc3fd6fc3ef081866ad84bfbb3a029c4a38c0
sama <sghavamzadeh46@gmail.com>
2025-05-10 22:47:40 +0330

    feat(actions): fetch single product functionality

--- utils/actions.ts
+++ utils/actions.ts
@@ -1,4 +1,5 @@
 import db from "@/utils/db";
+import { redirect } from "next/navigation";
 
 export const featuredProducts = async () => {
   const products = await db.product.findMany({
@@ -9,17 +10,29 @@ export const featuredProducts = async () => {
   return products;
 };
 
-export const fetchAllProducts = async ({search = ''}:{search:string}) => {
+export const fetchAllProducts = async ({ search = "" }: { search: string }) => {
   const products = await db.product.findMany({
-    where:{
-      OR:[
-        {name: {contains: search , mode: 'insensitive'}},
-        {company: {contains: search , mode: 'insensitive'}}
-      ]
+    where: {
+      OR: [
+        { name: { contains: search, mode: "insensitive" } },
+        { company: { contains: search, mode: "insensitive" } },
+      ],
     },
     orderBy: {
       createdAt: "desc",
     },
   });
   return products;
+};
+
+export const fetchSingleProduct = async (productId: string) => {
+  const product = await db.product.findUnique({
+    where: {
+      id: productId,
+    },
+  });
+  if (!product) {
+    redirect("/");
+  }
+  return product;
 };
